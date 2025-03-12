5c85a870a921cbe9f4e6d8c228197ea52409b053
Michael McCormack <mcctwin@gmail.com>
2025-03-11 20:23:03 -0500

    2 tests passed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1")
+document.body.append(newHeader)
+
+newHeader.id = "victory"
+console.log(newHeader.id)
+
+newHeader.textContent = "Michael is the champion"
